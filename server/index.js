48e33b022d04f5097ee330f9878c2dc8beda0ed6
lijun21 <wang283808065@gmail.com>
2018-05-16 23:46:40 -0700

    add config ignore file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 //commenJS modules
 const express = require('express');
+const passport = require('passport');
+
+require('./services/passport');
+
 const app = express();
 
-  app.get('/', (req, res) => {
-    res.send("hello world");
-  });
+app.get('/auth/google', 
+    passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
