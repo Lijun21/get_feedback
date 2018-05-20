29692b0d5173345d7eb4fb07bd877bea2227dcd3
lijun21 <wang283808065@gmail.com>
2018-05-19 20:12:41 -0700

    tweak google strategy settings

--- server/services/passport.js
+++ server/services/passport.js
@@ -26,7 +26,8 @@ passport.use(
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
-        // proxy: true
+        // hey google strategy, if our request run through any proxy, that's fine, just trust it and calculate the call back URL correctly, https not http
+        proxy: true
       },
     //  accessToken is the key to get access to the same google account
     //refreshToken refresh accessToken, accessToken expires after certain amount of time
