f31a95614cb86b0a4c302fb9f984078bbaecaf83
lijun21 <wang283808065@gmail.com>
2018-06-28 00:59:27 -0700

    redux form validata

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+
 
 const FIELDS = [
     { label: 'Survey Title', name: 'title' },
@@ -31,13 +33,30 @@ class SurveyForm extends Component {
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}
                 >
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to='/surveys' className="red btn-flat white-text">Cancel</Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                    Next
+                    <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         )
     }
 }
 
+function validate(values){
+    const errors = {};
+    // values.name return the key
+    // values[name] reutrn the value on the fly
+    _.forEach(FIELDS, ({ name }) => {
+        if (!values[name]){
+            errors[name] = `*You must provide a ${name}`;
+        }
+    })
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm);
